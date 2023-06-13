e972c0468b0b5874f3efc1f55f8addc8d053e875
prasad123-hub <narkhedkarprasad@gmail.com>
2023-06-13 16:17:40 +0530

    Feat: Initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,107 @@
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { MotionA, MotionDiv, MotionP } from "@/components/client"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
+    <section>
+      <div className="relative isolate overflow-hidden bg-background">
+        <svg
+          className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top,white,transparent)]"
+          aria-hidden="true"
         >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
+          <defs>
+            <pattern
+              id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+              width={100}
+              height={100}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M.5 200V.5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+            <path
+              d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect
+            width="100%"
+            height="100%"
+            strokeWidth={0}
+            fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+          />
+        </svg>
+        <div
+          className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
+          aria-hidden="true"
         >
-          GitHub
-        </Link>
+          <div
+            className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-green-600 opacity-20"
+            style={{
+              clipPath:
+                "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto flex max-w-7xl justify-center px-6 pb-24 pt-10 text-center sm:pb-32 lg:flex lg:px-8 lg:py-32">
+          <section className="relative flex flex-col items-center justify-center gap-8 ">
+            <div className="flex flex-col items-center gap-4 text-center">
+              <MotionA
+                href="/blog/unveiling-the-new-website"
+                className="z-10 inline-flex gap-2 rounded-full bg-background px-4 py-2 text-sm text-[#95A8C6] transition hover:text-white"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.6 }}
+              >
+                View our top talents &rarr;
+              </MotionA>
+              <MotionDiv
+                className="relative max-w-4xl"
+                initial={{ opacity: 0, top: 20 }}
+                animate={{ opacity: 1, top: 0 }}
+                transition={{ duration: 0.8, ease: "easeOut" }}
+              >
+                <h1 className="bg-gradient-to-b from-gray-100 to-gray-500 bg-clip-text py-4 font-heading text-4xl font-semibold leading-[1.1] tracking-wide text-transparent md:text-5xl">
+                  Generate beautiful and customizable images of your code
+                  snippets.
+                </h1>
+              </MotionDiv>
+              <MotionDiv
+                className="relative mt-4 max-w-lg"
+                initial={{ opacity: 0, top: 20 }}
+                animate={{ opacity: 1, top: 0 }}
+                transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
+              >
+                <p className="text-base text-[#7F92AF]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
+                  nihil obcaecati. Possimus hic ea dolor placeat saepe,
+                  accusantium omnis! Vitae.
+                </p>
+              </MotionDiv>
+            </div>
+            {/* <MotionDiv
+            className="relative mt-4 flex flex-col gap-4 md:flex-row"
+            initial={{ opacity: 0, top: 20 }}
+            animate={{ opacity: 1, top: 0 }}
+            transition={{ duration: 0.4, delay: 0.4, ease: "easeOut" }}
+          >
+            <Button variant="outline" size="lg">
+              View Talents
+              <Icons.arrowRight className="ml-2 h-4 w-4 text-green-600" />
+            </Button>
+            <Button variant="outline" size="lg">
+              Explore Work
+              <Icons.arrowRight className="ml-2 h-4 w-4 text-green-600" />
+            </Button>
+          </MotionDiv> */}
+          </section>
+        </div>
       </div>
     </section>
   )
--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "Snap",
+  description: "Share Beautiful designed screenshots of your code snippets",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +10,8 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com",
+    github: "https://github.com",
+    docs: "",
   },
 }
